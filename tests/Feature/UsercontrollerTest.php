28edc6a3ba90e27649df6336cd93dbf2cb083274
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-05-16 20:07:26 +0200

    Sync

--- tests/Feature/UsercontrollerTest.php
+++ tests/Feature/UsercontrollerTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature;
 
 use App\User;
 use Faker\Factory;
+use Illuminate\Http\UploadedFile;
+use Storage;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -27,15 +29,19 @@ class UserControllerTest extends TestCase
     public function testUsersAreCreatedOk()
     {
         // Prepare
+        Storage::fake('local');
+
         $faker = Factory::create();
         $user = [
             'name' => $name = $faker->name,
             'email' => $email = $faker->unique()->safeEmail ,
-            'password' => $password = bcrypt('secret')
+            'password' => $password = bcrypt('secret'),
+            'file' => UploadedFile::fake()->image('guapo.png')
         ];
         //Login as authorized user and Execute
         $authorizedUser = factory(User::class)->create();
         $response = $this->actingAs($authorizedUser,'api')->json('POST', 'api/v1/user', $user);
+//        ->dump();
 
         // Assert
         $response
@@ -47,7 +53,10 @@ class UserControllerTest extends TestCase
         $this->assertDatabaseHas('users', [
             'name' => $name,
             'email' => $email ,
-            'password' => $password ,
+            'password' => $password,
+            'file' => 'guapo.png',
         ]);
+
+        Storage::disk('local')->assertExists('guapo.png');
     }
 }
